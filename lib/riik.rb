18400a3718003615fccb65b38048b08a2cb9e19b
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2011-11-28 21:20:28 -0500

    Autoload document.

--- lib/riik.rb
+++ lib/riik.rb
@@ -1,28 +1,6 @@
 require 'riik/version'
 require 'riak'
 
-# Riik is a lightweight object mapper for Riak.
-#
-# Specify a class and include the object mapper mixin.  Then, specify the
-# argument list for the initialize method.
-#
-# ```ruby
-# class Person
-#   include Riik::Document
-#   initializes_with :first_name, :last_name
-# end
-# ```
-#
-# Then, use it!
-#
-# ```ruby
-# Person.find('this-key-name')  # => #<Person.. @first_name="Fat", @last_name="Mike">
-# p = Person.new('Fat', 'Mike') # => #<Person.. @first_name="Fat", @last_name="Mike">
-# p.save # => true
-# ```
-#
 module Riik
-  autoload :Configuration, 'riik/configuration'
-  autoload :Persistence,   'riik/persistence'
-  autoload :Document,      'riik/document'
+  autoload :Document, 'riik/document'
 end
